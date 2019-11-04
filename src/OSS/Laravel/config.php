48eed14e7c6a60a27142c33ff4e18a896fb68fd7
Hotcha <hotcha.py@gmail.com>
2019-11-04 18:45:24 +0800

    ADD: cdn_endpoint

--- src/OSS/Laravel/config.php
+++ src/OSS/Laravel/config.php
@@ -1,8 +1,9 @@
 <?php
 
 return [
-    'id'       => env('OSS_ACCESS_KEY_ID'),
-    'key'      => env('OSS_ACCESS_KEY_SECRET'),
-    'endpoint' => env('OSS_ENDPOINT'),
-    'bucket'   => env('OSS_BUCKET'),
+    'id'           => env('OSS_ACCESS_KEY_ID'),
+    'key'          => env('OSS_ACCESS_KEY_SECRET'),
+    'endpoint'     => env('OSS_ENDPOINT'),
+    'cdn_endpoint' => env('OSS_CDN_ENDPOINT'),
+    'bucket'       => env('OSS_BUCKET'),
 ];
